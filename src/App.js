69701fc1b49d4c92b7a01bf88f8ad940c88bcf93
Felix Ding <felixding1989@gmail.com>
2019-10-17 15:44:37 -0700

    added router for sub directory deployment

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import PathfindingVisualizer from './PathfindingVisualizer/PathfindingVisualizer';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <PathfindingVisualizer />
-    </div>
+    <Router>
+      <div className="App">
+        <Route path="/projects/Pathfinding" component={PathfindingVisualizer} />
+      </div>
+    </Router>
   );
 }
 
